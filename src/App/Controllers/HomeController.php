49c4e9556ffbdd953bd8435d880ff143c26789c1
Eliza Teplova <baboteplova@yandex.ru>
2024-01-26 00:15:10 +0300

    Router was defined

---
+++ src/App/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class HomeController
+{
+    public function home(): void
+    {
+        echo 'home page';
+    }
+}
+// "App\Controllers\HomeController"
+// "App\Controllers\HomeController.php"
---
+++ src/App/functions.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+function dd(mixed $value) : void {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die;
+}
