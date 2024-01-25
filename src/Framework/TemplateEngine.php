1a321e54c483f67364dd2aafe84ef2a47e283853
Eliza Teplova <baboteplova@yandex.ru>
2024-01-26 01:36:26 +0300

    Template Engine was added. Views as htmls were added as well

---
+++ src/Framework/TemplateEngine.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+class TemplateEngine
+{
+
+    public function __construct(private string $basePath)
+    {
+    }
+
+    public function render(string $template, array $data = []) //: void
+    {
+        extract($data, EXTR_SKIP);
+        ob_start(); // Turn on output buffering. The contend won't sent unless we say it directly or limit 4096 would be violated
+
+        include $this->resolve($template);
+        $output = ob_get_contents(); // Return the content of the output buffer
+        ob_end_clean(); // Clear ouput buffer and turn off output buffering
+
+        return $output;
+    }
+
+    public function resolve(string $path)
+    {
+        return "{$this->basePath}/$path";
+    }
+}
