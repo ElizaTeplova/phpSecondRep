e0cc28ed8c181c816b6220ded10c59391ebafc29
Eliza Teplova <baboteplova@yandex.ru>
2024-02-01 16:48:38 +0300

    Form validation was added (without sessions). Middleware for catching form errors was added too

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -11,7 +11,6 @@ class HomeController
 {
     public function __construct(private TemplateEngine $view)
     {
-        // $this->view = new TemplateEngine(Paths::VIEW);
     }
 
     public function home(): void
@@ -21,5 +20,3 @@ class HomeController
         ]);
     }
 }
-// "App\Controllers\HomeController"
-// "App\Controllers\HomeController.php"
--- src/Framework/Exceptions/ContainerExceptions.php
+++ src/Framework/Exceptions/ContainerExceptions.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Framework\Exeptions;
+namespace Framework\Exceptions;
 
 use Exception;
 
