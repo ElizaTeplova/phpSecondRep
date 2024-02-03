f6543d3742e45b77aa1cd6b5acf92803cb5fb907
Eliza Teplova <baboteplova@yandex.ru>
2024-02-03 12:23:00 +0300

    Dotenv was added. Composer was a bit modified. For branch sake

--- cli.php
+++ cli.php
@@ -1,14 +1,26 @@
 <?php
 
-include __DIR__ . "/src/Framework/Database.php";
+require __DIR__ . "/vendor/autoload.php";
+// include __DIR__ . "/src/Framework/Database.php";
+include __DIR__ . "/vendor/vlucas/phpdotenv/src/Dotenv.php";
 
 use Framework\Database;
+use Dotenv\Dotenv;
 
-$db = new Database('mysql', [
-    'host' => 'localhost',
-    'port' => 3306,
-    'dbname' => 'db_edu'
-], 'root', '');
+$dotenv = Dotenv::createImmutable('./');
+$dotenv->load();
+
+$db = new Database($_ENV['DB_DRIVER'], [
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'dbname' => $_ENV['DB_NAME']
+], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+
+// $db = new Database('mysql', [
+//     'host' => 'localhost',
+//     'port' => 3306,
+//     'dbname' => 'db_edu'
+// ], 'root', '');
 
 $sqlFile = file_get_contents('./database.sql');
 
--- src/App/Config/Paths.php
+++ src/App/Config/Paths.php
@@ -6,4 +6,5 @@ class Paths
 {
     public const VIEW = __DIR__ . "/../views";
     public const SOURCE = __DIR__ . "/../../";
+    public const ROOT = __DIR__ . '/../../../';
 }
