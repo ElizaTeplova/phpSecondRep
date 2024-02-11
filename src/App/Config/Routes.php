d62e0447a523671394ac27454ef3d9cff2b0203e
Eliza Teplova <baboteplova@yandex.ru>
2024-02-12 00:20:53 +0300

    File uploading partially added

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -9,7 +9,8 @@ use App\Controllers\{
     HomeController,
     AboutController,
     AuthController,
-    TransactionController
+    TransactionController,
+    ReceiptController
 };
 
 use App\Middleware\{AuthRequiredMiddleware, GuestOnlyMiddleware};
@@ -23,9 +24,11 @@ function registerRoutes(App $app)
     $app->get('/login', [AuthController::class, 'loginView'])->add(GuestOnlyMiddleware::class);
     $app->post('/login', [AuthController::class, 'login'])->add(GuestOnlyMiddleware::class);
     $app->get('logout', [AuthController::class, 'logout'])->add(AuthrequiredMiddleware::class);
-    $app->get('/transaction', [TransactionController::class, 'createView'])->add(AuthRequiredMiddleware::class);
-    $app->post('/transaction', [TransactionController::class, 'create'])->add(AuthRequiredMiddleware::class);
-    $app->get('/transaction/{transaction}', [TransactionController::class, 'editView'])->add(AuthRequiredMiddleware::class);
-    $app->post('/transaction/{transaction}', [TransactionController::class, 'edit'])->addMiddleware(AuthRequiredMiddleware::class);
-    $app->delete("/transaction/{transaction}", [TransactionController::class, 'delete'])->addMiddleware(AuthRequiredMiddleware::class);
+    $app->get('/transaction', [TransactionController::class, 'createView']);
+    $app->post('/transaction', [TransactionController::class, 'create']);
+    $app->get('/transaction/{transaction}', [TransactionController::class, 'editView']);
+    $app->post('/transaction/{transaction}', [TransactionController::class, 'edit']);
+    $app->delete("/transaction/{transaction}", [TransactionController::class, 'delete']);
+    $app->get('/transaction/{transaction}/receipt', [ReceiptController::class, 'uploadView']);
+    $app->post('/transaction/{transaction}/receipt', [ReceiptController::class, 'upload']);
 }
