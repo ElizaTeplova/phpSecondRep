5c545581c3a966c2dab448b84b52483f409cdc41
Eliza Teplova <baboteplova@yandex.ru>
2024-02-13 01:37:21 +0300

    File uploading and downloading were added in ReceiptController/Service

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -24,11 +24,13 @@ function registerRoutes(App $app)
     $app->get('/login', [AuthController::class, 'loginView'])->add(GuestOnlyMiddleware::class);
     $app->post('/login', [AuthController::class, 'login'])->add(GuestOnlyMiddleware::class);
     $app->get('logout', [AuthController::class, 'logout'])->add(AuthrequiredMiddleware::class);
-    $app->get('/transaction', [TransactionController::class, 'createView']);
-    $app->post('/transaction', [TransactionController::class, 'create']);
-    $app->get('/transaction/{transaction}', [TransactionController::class, 'editView']);
-    $app->post('/transaction/{transaction}', [TransactionController::class, 'edit']);
-    $app->delete("/transaction/{transaction}", [TransactionController::class, 'delete']);
-    $app->get('/transaction/{transaction}/receipt', [ReceiptController::class, 'uploadView']);
-    $app->post('/transaction/{transaction}/receipt', [ReceiptController::class, 'upload']);
+    $app->get('/transaction', [TransactionController::class, 'createView'])->add(AuthrequiredMiddleware::class);;
+    $app->post('/transaction', [TransactionController::class, 'create'])->add(AuthrequiredMiddleware::class);;
+    $app->get('/transaction/{transaction}', [TransactionController::class, 'editView'])->add(AuthrequiredMiddleware::class);;
+    $app->post('/transaction/{transaction}', [TransactionController::class, 'edit'])->add(AuthrequiredMiddleware::class);;
+    $app->delete("/transaction/{transaction}", [TransactionController::class, 'delete'])->add(AuthrequiredMiddleware::class);;
+    $app->get('/transaction/{transaction}/receipt', [ReceiptController::class, 'uploadView'])->add(AuthrequiredMiddleware::class);;
+    $app->post('/transaction/{transaction}/receipt', [ReceiptController::class, 'upload'])->add(AuthrequiredMiddleware::class);;
+    $app->get('/transaction/{transaction}/receipt/{receipt}', [ReceiptController::class, 'download'])->add(AuthrequiredMiddleware::class);;
+    $app->delete('/transaction/{transaction}/receipt/{receipt}', [ReceiptController::class, 'delete'])->add(AuthrequiredMiddleware::class);;
 }
